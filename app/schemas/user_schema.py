0b28a41ac350b67ce8326a425e4eaa6c6da7422f
matteofredi <m.fredi@codermine.com>
2023-10-20 17:02:46 +0200

    [feature] BE implemented user's crud

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -9,3 +9,11 @@ class User(BaseModel):
 
     class Config:
         from_attributes = True
+
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+    class Config:
+        from_attributes = True
