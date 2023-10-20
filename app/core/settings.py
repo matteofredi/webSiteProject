c8f2b4397c771f5581e8ffcd984bc367c62f8e93
matteofredi <m.fredi@codermine.com>
2023-10-20 13:36:05 +0200

    [feature] BE added ALLOWED_HOST as a env variable

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,4 +1,6 @@
+import ast
 import os
+from typing import List
 
 from dotenv import load_dotenv
 
@@ -6,7 +8,8 @@ load_dotenv()
 
 
 class Settings:
-    APP_BASE_URL = os.getenv(key='APP_BASE_URL', default=None)
+    APP_BASE_URL: str = os.getenv(key='APP_BASE_URL', default=None)
+    ALLOWED_HOST: List[str] = ast.literal_eval(os.getenv(key='ALLOWED_HOST', default=None))
 
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -12,22 +12,23 @@ app = FastAPI()
 app.mount('/static', staticfiles.StaticFiles(directory='app/static'), name='static')
 
 
+@functools.lru_cache
+def get_settings():
+    return settings
+
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=get_settings().ALLOWED_HOST,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-@functools.lru_cache
-def get_settings():
-    return settings
-
-
 @app.get(path='/', status_code=200)
 def root():
+    print(type(get_settings().ALLOWED_HOST))
     return RedirectResponse(url=f'{get_settings().APP_BASE_URL}/login')
 
 
